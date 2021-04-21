1d9c0db566f3842bd2124838b0874315ebb91175
qiuzhanghua <qiuzhanghua@icloud.com>
2021-04-21 15:29:24 +0800

    DataFrame Basics

---
+++ src/main/scala/part2dataframes/DataFrameBasics.scala
@@ -0,0 +1,73 @@
+package com.example
+package part2dataframes
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+
+object DataFrameBasics extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DataFrames Basics")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val firstDF = spark.read
+    .format("json")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/cars.json")
+
+  firstDF.show()
+  firstDF.printSchema()
+  firstDF.take(10).foreach(println)
+
+  val carsSchema = StructType(
+    Array(
+      StructField("Name", StringType),
+      StructField("Miles_per_Gallon", IntegerType),
+      StructField("Cylinders", IntegerType),
+      StructField("Displacement", IntegerType),
+      StructField("Horsepower", IntegerType),
+      StructField("Weight_in_lbs", IntegerType),
+      StructField("Acceleration", DoubleType),
+      StructField("Year", DateType),
+      StructField("Origin", StringType),
+    )
+  )
+
+  val carsDFSchema = firstDF.schema
+  println(carsDFSchema)
+  println(carsSchema)
+
+  val carsDFWithSchema = spark.read
+    .format("json")
+    .schema(carsSchema)
+    .load("src/main/resources/data/cars.json")
+
+  carsDFWithSchema.show()
+
+  val myRow = Row("chevrolet chevelle malibu", 18.0, 8L, 307.0, 130L, 3504L, 12.0, "1970-01-01", "USA");
+
+  val cars = Seq(
+    ("chevrolet chevelle malibu", 18.0, 8L, 307.0, 130L, 3504L, 12.0, "1970-01-01", "USA"),
+    ("buick skylark 320", 15.0, 8L, 350.0, 165L, 3693L, 11.5, "1970-01-01", "USA"),
+    ("plymouth satellite", 18.0, 8L, 318.0, 150L, 3436L, 11.0, "1970-01-01", "USA"),
+    ("amc rebel sst", 16.0, 8L, 304.0, 150L, 3433L, 12.0, "1970-01-01", "USA"),
+    ("ford torino", 17.0, 8L, 302.0, 140L, 3449L, 10.5, "1970-01-01", "USA"),
+    ("ford galaxie 500", 15.0, 8L, 429.0, 198L, 4341L, 10.0, "1970-01-01", "USA"),
+    ("chevrolet impala", 14.0, 8L, 454.0, 220L, 4354L, 9.0, "1970-01-01", "USA"),
+    ("plymouth fury iii", 14.0, 8L, 440.0, 215L, 4312L, 8.5, "1970-01-01", "USA"),
+    ("pontiac catalina", 14.0, 8L, 455.0, 225L, 4425L, 10.0, "1970-01-01", "USA"),
+    ("amc ambassador dpl", 15.0, 8L, 390.0, 190L, 3850L, 8.5, "1970-01-01", "USA")
+  )
+
+  val manualCarsDF = spark.createDataFrame(cars)
+
+  manualCarsDF.show()
+
+  import spark.implicits._
+
+  val manualCarsDFWithImplicits = cars.toDF("Name", "MGP", "Cylinders", "Displacement", "HP", "Weight", "Acceleration", "Year", "Country")
+
+  manualCarsDFWithImplicits.show()
+
+}
